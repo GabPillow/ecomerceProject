4c251f2e819773d26205af573a424f45da3fbd48
Gabrielle Rochefort <gabrochefort@gmail.com>
2025-02-14 11:41:01 +0100

    (feat) extract game for show

--- ecommerce-backend/app/controllers/api/v1/games_controller.rb
+++ ecommerce-backend/app/controllers/api/v1/games_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::GamesController < Api::V1::BaseController
+  before_action :set_game, only: [ :show ]
+
   def index
     @games = policy_scope(Game)
   end
+
+  def show
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
+    authorize @game  # For Pundit
+  end
+
 end
