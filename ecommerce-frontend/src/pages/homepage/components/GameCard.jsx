a288fd0c0dfb41818731000f6bb40f11daee44af
Gabrielle Rochefort <gabrochefort@gmail.com>
2025-02-14 15:20:10 +0100

    show front

--- ecommerce-frontend/src/pages/homepage/components/GameCard.jsx
+++ ecommerce-frontend/src/pages/homepage/components/GameCard.jsx
@@ -1,15 +1,18 @@
 import '../homepage.css'
+import { Link } from 'react-router-dom';
 
 function GameCard({ game }) {
   return (
-    <div className='card-game'>
-      <img src={`/images/${game.image}`} alt="" className="image"></img>
-      <div className='title'>
-        <div className='box-price'>
-          {game.price}$
+    <Link to={`/game/${game.id}`}>
+      <div className='card-game'>
+        <img src={`/images/${game.image}`} alt="" className="image"></img>
+        <div className='title'>
+          <div className='box-price'>
+            {game.price}$
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
